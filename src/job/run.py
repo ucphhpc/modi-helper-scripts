bcd8f70f4f8c79fc9a914dd02e10bb76c308b5ac
Rasmus Munk <munk1@live.dk>
2023-06-02 17:11:32 +0200

    Introduce the scratch space directory argument and add additional checks that the user has use valid paths

--- src/job/run.py
+++ src/job/run.py
@@ -1,6 +1,6 @@
 from utils.job import run
 
 
-def run_job(job_file, runtime_directory):
+def run_job(runtime_directory, job_file):
     command = ["sbatch", job_file]
     return run(command, cwd=runtime_directory)
--- src/submit_job.py
+++ src/submit_job.py
@@ -2,32 +2,64 @@ import click
 import os
 from job.initialize import check_job_paths
 from job.run import run_job
-from utils.io import exists
+from utils.io import exists, expanduser
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.argument("job_file")
+@click.argument("job-file")
 @click.option(
     "--runtime-directory",
     "-rd",
     default=os.path.join("~", "modi_mount"),
-    help="The path to the runtime directory in which the job is to be executed",
+    help="""
+    The path to the runtime directory in which the job is to be executed.
+    This directory must be within the scratch space directory.
+    """,
 )
-def main(job_file, runtime_directory):
+@click.option(
+    "--scratch-space-directory",
+    "-ssd",
+    default=os.path.join("~", "modi_mount"),
+    help="""
+    The path to the scratch space directory.
+    """,
+)
+def main(job_file, runtime_directory, scratch_space_directory):
+    job_file = expanduser(job_file)
+    runtime_directory = expanduser(runtime_directory)
+    scratch_space_directory = expanduser(scratch_space_directory)
+
     if not exists(job_file):
         print(
-            "Failed to find the job file: {} - are you sure it exists?".format(job_file)
+            "Failed to find the job-file: {} - are you sure it exists?".format(job_file)
         )
         exit(-1)
 
-    correct_paths = check_job_paths(runtime_directory, job_file)
-    if not correct_paths:
+    if not exists(runtime_directory):
+        print("The specified runtime directory does not exist.")
+        exit(-1)
+
+    if not exists(scratch_space_directory):
+        print("The specified scratch space directory does not exist.")
+        exit(-1)
+
+    correct_directories = check_job_paths(scratch_space_directory, runtime_directory)
+    if not correct_directories:
         print(
-            "Your `job_file`: {} must reside inside the scratch_space directory: {}".format(
+            "Your `runtime-directory`: {} must reside inside the `scratch-space-directory`".format(
+                runtime_directory, scratch_space_directory
+            )
+        )
+        exit(-2)
+
+    correct_job = check_job_paths(runtime_directory, job_file)
+    if not correct_job:
+        print(
+            "Your `job-file`: {} must reside inside the `runtime-directory`: {}".format(
                 job_file, runtime_directory
             )
         )
-        exit(-3)
+        exit(-2)
 
     job_output = run_job(job_file, runtime_directory)
     if job_output["returncode"] != "0":
