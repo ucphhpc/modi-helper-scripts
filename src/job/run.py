c6f2a55311d0a04f0754b9892874bdcb5c67ce8b
Rasmus Munk <munk1@live.dk>
2023-06-03 16:07:45 +0200

    Condition the args extend on whether it is set or not

--- src/job/run.py
+++ src/job/run.py
@@ -3,5 +3,6 @@ from utils.job import run
 
 def run_job(runtime_directory, job_file, *args, **kwargs):
     command = ["sbatch", job_file]
-    command.extend(args)
+    if args:
+        command.extend(*args)
     return run(command, cwd=runtime_directory)
