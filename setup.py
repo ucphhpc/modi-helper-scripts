fc5dc585cb17aa80d8399be5948b47a76ae301ca
Rasmus Munk <munk1@live.dk>
2023-11-23 14:07:52 +0100

    Add the missing r to modi-list-environments

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
             "modi-new-environment=modi_helper.cli.environment_new:main",
             "modi-delete-environment=modi_helper.cli.environment_delete:main",
             "modi-load-environments=modi_helper.cli.environments_load:main",
-            "modi-list-envionments=modi_helper.cli.environments_list:main",
+            "modi-list-environments=modi_helper.cli.environments_list:main",
         ]
     },
     classifiers=[
